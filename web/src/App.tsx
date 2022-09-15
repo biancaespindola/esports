83c33708f84b024812ba3e3245befc8c3aed7a21
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-09-15 20:45:38 -0300

    a base do react

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+// Components / property
+interface ButtonProps {
+	title: string;
+}
+
+function Button(props: ButtonProps) {
+	return <button>{props.title}</button>;
+}
+
+function App() {
+	return (
+		<div>
+			<Button title="Send 1" />
+			<Button title="Send 2" />
+			<Button title="Send 3" />
+			<Button title="outro botao atualizando" />
+		</div>
+	);
+}
+
+export default App;
